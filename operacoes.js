52005cbd86f652338b30191f05de71fb99d4bcc2
Wellington <w_santos1993@outlook.com>
2020-08-13 12:02:37 -0300

    Tipos de operacões que podem ser utilizadas em JS

---
+++ operacoes.js
@@ -0,0 +1,74 @@
+// Tipos de operacões
+
+// Adicão
+console.log(2 + 2);
+
+// Subtracão
+console.log(5 - 3);
+
+// Multiplicacão
+console.log(2 * 3);
+
+// divisão
+console.log(20 / 5);
+
+// Incremento
+let numero = 20;
+console.log(numero ++); // executa primeiro o console e depois incrementa, sendo necessário um novo console.log.
+console.log(numero);
+
+// Outra forma de incremento
+
+let num = 12;
+console.log(++num); // incrementa e depois executa o console.
+
+// Decremento
+let numerico = 15;
+console.log(-- numerico); // diminue o valor numerico.
+
+// Comparacão >.
+console.log(7 > 6);
+
+// comparacão <.
+console.log(7 < 6);
+
+// comparacão < ou =.
+console.log(3 <= 3);
+
+// comparacão > ou =.
+console.log(20 >= 20);
+
+// comparacão ==.
+console.log(10 == 10); 
+
+// comparacão == rígido.
+console.log(7 === '7'); // os valores são iguals, exceto o tipo
+
+console.log(7 === 7); // os valores são iguais.
+
+// comparacão diferente.
+console.log(10 != 20); // retorna valor true pois os valores são diferentes.
+
+// comparacão diferente rígido.
+console.log(10 !== '10'); // retorna true, pois os valores são iguais, mas o tipo é diferente.
+
+// comparacão lógica &&.
+console.log (9 + 7 && 7 < 8); // retorna true se ambas comparacões forem verdadeiras
+
+// comparacão lógica ||
+console.log(5 > 4 || 5 < 3); // retorna true se ao menos 1 for verdadeiro, se ambos não corresponderem ao inunciado retornará false.
+
+// comparacão negacão !.
+console.log(!6 > 4);
+                        // sempre altera contrariando o valor boleano. ex: é maior mas torna false, não é menor, mas torna true.
+console.log(!8 < 7);
+
+// concatenar
+let nome = "Wellingotn";
+let sobrenome = "Santos";
+
+console.log(nome + " " + sobrenome);
+
+
+
+
